9960fa6423b66307d96d42b87b16fb3fe3011e78
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2025-02-21 16:55:35 +0200

    creat main functiion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ResponsiveDashboard());
+}
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
